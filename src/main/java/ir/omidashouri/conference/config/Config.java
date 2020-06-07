eeead5659d90e0e4d2c819a6c6ef7d1f6b4c16ba
o.ashouri <omidashouri@gmail.com>
2020-06-07 14:59:03 +0430

    i18N part three : 07.03 : omidashouri : 99.03.18

--- src/main/java/ir/omidashouri/conference/config/Config.java
+++ src/main/java/ir/omidashouri/conference/config/Config.java
@@ -1,15 +1,26 @@
 package ir.omidashouri.conference.config;
 
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.i18n.LocaleContext;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import java.util.Locale;
 
 @Configuration
 public class Config implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry resourceHandlerRegistry){
+        resourceHandlerRegistry.addResourceHandler("/files/**")
+                .addResourceLocations("/WEB-INF/pdf");
+    }
 
   @Bean
   public ViewResolver viewResolver(){
@@ -20,10 +31,24 @@ public class Config implements WebMvcConfigurer {
       return bean;
   }
 
+
+  @Bean
+  public LocaleResolver localResolver(){
+      SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+      sessionLocaleResolver.setDefaultLocale(Locale.US);
+      return sessionLocaleResolver;
+  }
+
+  @Bean
+  public LocaleChangeInterceptor localeChangeInterceptor(){
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        return localeChangeInterceptor;
+  }
+
   @Override
-  public void addResourceHandlers(ResourceHandlerRegistry resourceHandlerRegistry){
-      resourceHandlerRegistry.addResourceHandler("/files/**")
-              .addResourceLocations("/WEB-INF/pdf");
+  public void addInterceptorHandlers(InterceptorRegistry interceptorRegistry){
+        interceptorRegistry.addInterceptor(localeChangeInterceptor());
   }
 
 
