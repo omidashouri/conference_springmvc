07893a5f68674792429b163cb800a4e8ef8fab35
o.ashouri <omidashouri@gmail.com>
2020-06-11 13:08:31 +0430

    new controller : omidashouri : 99.03.22

--- src/main/java/ir/omidashouri/conference/controller/UserController.java
+++ src/main/java/ir/omidashouri/conference/controller/UserController.java
@@ -1,6 +1,7 @@
 package ir.omidashouri.conference.controller;
 
 import ir.omidashouri.conference.model.User;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
